cad4858db7cac31b2c26b4c307c02ab7af5de8d8
Volodymyr Rudyi <volodymyr@syncmyorders.com>
2026-05-25 10:03:06 +0200

    Remove Storybook from frontend

--- crates/runtara-server/frontend/src/features/workflows/components/WorkflowEditor/CustomNodes/storybook/index.ts
+++
@@ -1,9 +0,0 @@
-export { ReactFlowStoryWrapper } from './ReactFlowStoryWrapper';
-export {
-  resetStores,
-  setStoreEdges,
-  setNodeUnsaved,
-  setNodeValidationError,
-  setNodeExecutionStatus,
-  setExecuting,
-} from './store-helpers';
